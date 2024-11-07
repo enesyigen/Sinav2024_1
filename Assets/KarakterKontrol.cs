f4fd2a207405694f837a6c011381bb60aed136af
enesyigen <enesyigen01@gmail.com>
2024-11-07 13:07:04 +0300

    Update KarakterKontrol.cs

--- Assets/KarakterKontrol.cs
+++ Assets/KarakterKontrol.cs
@@ -1,11 +1,13 @@
 using System;
 using TMPro;
 using UnityEngine;
+using TriggerTag.Engel;
+using TriggerTag.Puan;
 
 public class KarakterKontrol : MonoBehaviour
 {
-    // Ad Soyad: 
-    // Öğrenci Numarası: 
+    // Ad Soyad: Meriç Armağan Yücel
+    // Öğrenci Numarası: 232011023
 
 
     // Soru 1: Karakteri yön tuşları ile hareket ettiren kodu, HareketEt fonksiyonu içerisine yazınız.
@@ -32,11 +34,23 @@ public class KarakterKontrol : MonoBehaviour
     void Update()
     {
         // Yazdığınız metodları çağırınız.
+        HareketEt();
+        Zipla();
+        OnTriggerEnter2D();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Soru 3 ve soru 4 burada çözülecek.
+        if(karakterRb(x,y,z)==TriggerTag.Engel(x,y,z))
+        {
+            TMP_Text metin="Oyun Bitti!";
+            transform.Vector3(0,0,0);
+        }
+        else if(karakterRb(x,y,z)==TriggerTag.Puan(x,y,z))
+        {
+            skor++;
+        }
     }
 
     void Zipla()
@@ -44,8 +58,19 @@ public class KarakterKontrol : MonoBehaviour
         // Space tuşuna basınca karakter zıplamalı ancak aşağıdaki kod hatalı.
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 ziplamaYonu = new Vector3(UnityEngine.Random.Range(-1f, 1f), 1, UnityEngine.Random.Range(-1f, 1f));
-            karakterRb.AddForce(ziplamaYonu * (ziplamaGucu / 2), ForceMode2D.Impulse);
+            Vector3 ziplamaYonu = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f));
+            karakterRb.AddForce(UnityEngine.Vector3.ziplamaYonu * (ziplamaGucu / 2), ForceMode2D.Impulse);
+        }
+    }
+    void HareketEt()
+    {
+        if(input.GetKey(KeyCode.LeftArrow))
+        {
+            karakterRb.AddForce(UnityEngine.Vector3.Left*hiz);
+        }
+        else if(input.GetKey(KeyCode.RightArrow))
+        {
+            karakterRb.AddForce(UnityEngine.Vector3.Right*hiz);
         }
     }
 }
